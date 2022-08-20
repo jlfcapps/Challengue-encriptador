5d1624b3afcbd5ac326853d932195f7a9d978237
JOSE LUIS FLOREZ CASTRO <jlfc1988@gmail.com>
2022-08-19 22:07:19 -0500

    Encriptador

---
+++ app.js
@@ -0,0 +1,121 @@
+var entrada = document.querySelector('#entrada');
+var botonEncriptar = document.querySelector('#encriptar');
+var botonDesencriptar = document.querySelector('#desencriptar');
+var barra = document.querySelector('.barra-lateral');
+var clipboard = document.querySelector('.copiar');
+var copyText = document.querySelector('.copy-text');
+var salida
+var conta = 0;
+entrada.focus();
+
+function toggleDarkLight() {
+    var element = document.body;
+    var element1 = document.querySelector('.foot');
+    element.classList.toggle("dark-mode");
+    conta++;
+    if(conta === 2){
+        conta = 0;
+    } else{
+        conta = 1;
+    }
+    conta === 1 ? element1.classList.add("dark-mode") : element1.classList.remove("dark-mode");   
+}
+
+function laSalida() {
+    const elem = document.querySelector('#salida');
+    const hijo = document.createTextNode('');
+
+    elem.innerHTML = ''
+    elem.appendChild(hijo);
+    salida = hijo;
+}
+
+laSalida();
+
+function codificador(item){
+    switch(item) {
+        case 'a': return 'ai'
+        case 'e': return 'enter'
+        case 'i': return 'imes'
+        case 'o': return 'ober'
+        case 'u': return 'ufat'
+        default: return item
+    }
+}
+
+function iteraCode(txt){
+    txt = entrada.value.toLowerCase();
+    var t = '';
+    for(var i = 0; i < txt.length; i++) {
+        t += codificador(txt[i]);
+    }
+    return t;
+}
+
+function iteraDeCode(txt){
+    txt = entrada.value.toLowerCase();
+    return txt.replace(/ai|enter|imes|ober|ufat/gi, function(matched){
+        switch(matched) {
+            case 'ai': return 'a'
+            case 'enter': return 'e'
+            case 'imes': return 'i'
+            case 'ober': return 'o'
+            case 'ufat': return 'u'
+            case ' ': return ''
+            default: return matched
+        }
+    });
+}
+
+botonEncriptar.addEventListener('click', function() {
+    var txt = entrada.value;
+    if(txt.length === 0) {
+        salida.nodeValue = ''
+        ocultarRes();
+    } else {
+        salida.nodeValue = iteraCode(txt).toLowerCase();
+        mostrarRes();
+    }
+});
+
+function mostrarRes(){
+    barra.classList.add('con-salida');
+}
+
+function ocultarRes(){
+    barra.classList.remove('con-salida');
+}
+
+botonDesencriptar.addEventListener('click', function() {
+    var valor = entrada.value;
+    if(valor.length === 0) {
+        salida.nodeValue= ''
+        ocultarRes();
+    } else {
+        salida.nodeValue = iteraDeCode(valor);
+        mostrarRes();
+    }
+    
+}); 
+
+entrada.addEventListener('input', function() {
+    var valor = entrada.value;
+    if(valor.length === 0) {
+        salida.nodeValue= ''
+        ocultarRes();
+    }
+
+});
+
+var clip = navigator.clipboard;
+
+clipboard.addEventListener('click', function(){
+    var input = salida.nodeValue;
+    if(clip){
+        clip.writeText(input);
+        copyText.classList.add('active');
+        setTimeout(function(){
+            copyText.classList.remove('active');
+        }, 2000);
+    }
+})
